884feaca2b1dd24e724a006291eb04bbfcd479ac
Pete Broderick <simplybinary@gmail.com>
2014-09-30 21:54:37 -0400

    Updates validations for game and person

--- game.rb
+++ game.rb
@@ -2,5 +2,7 @@ class Game < ActiveRecord::Base
   belongs_to :person
 
   validates :title, length: { minimum: 5, maximum: 50 }
+  validates :number_of_players, numericality: { only_integer: true }, presence: true
+  
 
 end
--- person.rb
+++ person.rb
@@ -1,3 +1,5 @@
 class Person < ActiveRecord::Base
   has_many :games
+
+  validates :name, length: { minimum: 1, maximum: 255 }
 end
